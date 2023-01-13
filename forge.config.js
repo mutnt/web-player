8600042545fc1b56ddeaf30ebd5b770ae227f32b
Xananax <me@xananax.com>
2023-01-13 17:16:53 +0100

    add windows and mac targets

--- forge.config.js
+++ forge.config.js
@@ -1,36 +1,31 @@
+const options = {
+  name: "Web Player",
+  id: "mutnt.directorybrowser",
+  categories: ["Network", "Utility"],
+  genericName: "Browser",
+  description: "Run local directories in an electron browser",
+  mimeType: ["text/html"],
+};
+
 module.exports = {
   packagerConfig: {},
   rebuildConfig: {},
   makers: [
-    /*
     {
-      name: '@electron-forge/maker-squirrel',
-      config: {},
+      name: "@electron-forge/maker-squirrel",
+      config: { options },
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
+      name: "@electron-forge/maker-dmg",
+      config: {
+        format: "ULFO",
+        options,
+      },
     },
-    {
-      name: '@electron-forge/maker-deb',
-      config: {},
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {},
-    },
-    */
     {
       name: "@electron-forge/maker-flatpak",
       config: {
-        options: {
-          name: "Web Player",
-          id: "mutnt.directorybrowser",
-          categories: ["Network", "Utility"],
-          genericName: "Browser",
-          description: "Run local directories in an electron browser",
-          mimeType: ["text/html"],
-        },
+        options,
       },
     },
   ],
